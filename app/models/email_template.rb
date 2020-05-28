c368fb963221fd090533970a8e895a9d47badd15
Matt McCormick <mmcc@jcdrepair.com>
2020-05-28 09:06:34 -0500

    Validate email template & spec test.

--- app/models/email_template.rb
+++ app/models/email_template.rb
@@ -1,3 +1,4 @@
 class EmailTemplate < ApplicationRecord
-
+  validates :name, :subject, :message, presence: true
+  validates :admin_id, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/email_template_spec.rb
+++ spec/models/email_template_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe EmailTemplate, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) { @template = EmailTemplate.new({ name: 'Some Template',
+                                                  subject: 'Some Subject',
+                                                  message: 'This is a message.',
+                                                  admin_id: 1
+                                                }) }
+  it 'valid' do
+    expect(@template.save).to eq(true)
+  end
+
+  context 'invalid' do
+    after(:each) { expect(@template.save).to eq(false) }
+
+    it 'name is nil' do
+      @template.name = nil
+    end
+
+    it 'name is blank' do
+      @template.name = ''
+    end
+
+    it 'subject is nil' do
+      @template.subject = nil
+    end
+
+    it 'subject is blank' do
+      @template.subject = ''
+    end
+
+    it 'message is nil' do
+      @template.message = nil
+    end
+
+    it 'message is blank' do
+      @template.message = ''
+    end
+
+    it 'admin_id is nil' do
+      @template.admin_id = nil
+    end
+
+    it 'admin_id is 0' do
+      @template.admin_id = 0
+    end
+  end
 end
