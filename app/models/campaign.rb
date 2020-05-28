f90fb768d3eaffbd49eb9d2e68bd6a2b2ff5a811
Matt McCormick <mmcc@jcdrepair.com>
2020-05-28 17:54:29 -0500

    Campaign validations and tests.

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,5 @@
 class Campaign < ApplicationRecord
   belongs_to :email_template
+
+  validates :admin_id, :email_template_id, numericality: { only_integer: true, greater_than: 0 }
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Campaign, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @template = EmailTemplate.create!({ name: 'Some Template',
+                                        subject: 'Some Subject',
+                                        message: 'This is a message.',
+                                        admin_id: 1
+                                      })
+    @campaign = Campaign.new({ email_template_id: @template.id, admin_id: 1 })
+  end
+
+  it 'valid' do
+    expect(@campaign.save).to eq(true)
+  end
+
+  context 'invalid' do
+    it 'email_template_id is nil' do
+      @campaign.email_template_id = nil
+      expect(@campaign).to_not be_valid
+      expect(@campaign.errors[:email_template_id]).to include('is not a number')
+    end
+
+    it 'email_template_id is 0' do
+      @campaign.email_template_id = 0
+      expect(@campaign).to_not be_valid
+      expect(@campaign.errors[:email_template_id]).to include('must be greater than 0')
+    end
+
+    it 'admin_id is nil' do
+      @campaign.admin_id = nil
+      expect(@campaign).to_not be_valid
+      expect(@campaign.errors[:admin_id]).to include('is not a number')
+    end
+
+    it 'admin_id is 0' do
+      @campaign.admin_id = 0
+      expect(@campaign).to_not be_valid
+      expect(@campaign.errors[:admin_id]).to include('must be greater than 0')
+    end
+  end
 end
