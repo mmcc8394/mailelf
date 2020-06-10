ec906a6d4f3898e4a2503c851d3d3e8f6bbd434e
Matt McCormick <mmcc@jcdrepair.com>
2020-06-09 21:33:13 -0500

    Getting Delayed_Job to restart on deployment.

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,35 +10,3 @@ set :log_level, :info
 
 set :rvm_type, :user
 set :rvm_ruby_version, 'ruby-2.6.3@bulkmailer'
-
-# Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/master.key'
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-#set :pty, true
-#set :ssh_options, {
-#    forward_agent: true,
-#    auth_methods: %w[publickey],
-#    keys: %w[/Users/Matt/.ssh/id_mailelf]
-#}
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
